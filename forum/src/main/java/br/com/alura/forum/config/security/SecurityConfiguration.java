cc579a5c207db36ecfa9cbde8b1b0185e64c79f4
paulosthiven25 <paulosthiven25gomes@gmail.com>
2022-06-04 18:11:34 -0300

    parte 7 finalizada,parte 2 do spring Boot REST finalizada

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -56,6 +56,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**","/configuration/**", "/swagger-resources/**");
+		
 	}
 	
 	
