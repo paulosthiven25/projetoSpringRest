769e16f6921102a85cc2329147e8b5f41ea50cde
paulosthiven25 <paulosthiven25gomes@gmail.com>
2022-06-06 15:58:40 -0300

    início da 3º parte do projeto spring boot REST,parte 1 e 2 finalizadas

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private AutenticacaoService as;
@@ -46,6 +48,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST,"/auth").permitAll()
 		.antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
+		.antMatchers("/h2-console/**").permitAll()
+		.antMatchers(HttpMethod.DELETE,"/topicos/*").hasRole("MODERADOR")
 		.anyRequest().authenticated()
 		.and().csrf().disable().
 		sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,5 +1,12 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com','$2a$10$ekyr6Any.P0Boxpz7Bi18uRflZzItgv1D.F1G3UCdrv6PYvmJ3jwW');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno','aluno@email.com','$2a$10$ekyr6Any.P0Boxpz7Bi18uRflZzItgv1D.F1G3UCdrv6PYvmJ3jwW');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador','moderador@email.com','$2a$10$ekyr6Any.P0Boxpz7Bi18uRflZzItgv1D.F1G3UCdrv6PYvmJ3jwW');
 
+
+INSERT INTO PERFIL(id,nome) VALUES (1,'ROLE_ALUNO');
+INSERT INTO PERFIL(id,nome) VALUES (2,'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id,perfis_id) VALUES (2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
